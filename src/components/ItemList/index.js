47e72046374ea15d2e0d249625dbae1a7f4398a6
karlkras <karlkras@yahoo.com>
2019-01-03 19:17:44 -0800

    list addjustments

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, } from 'react-native';
 import ListItem from '../ListItem';
 import styles from './styles';
 
@@ -7,7 +7,11 @@ import styles from './styles';
 const ItemList = ({ items }) => {
 
     const itemsOutput = items.map( (item, index) => (
-        <ListItem key={index} item={item}/>
+        <ListItem
+            key={index}
+            item={item}
+            onItemPressed={() => alert("Item pressed - ID: " + index)}
+        />
     ));
 
     return (
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-const listItem  = ({ item }) => (
-    <View style={styles.listItem}>
-        <Text>{item}</Text>
-    </View>
+const listItem  = ({ item, onItemPressed }) => (
+    <TouchableOpacity onPress={onItemPressed}>
+        <View style={styles.listItem}>
+            <Text>{item}</Text>
+        </View>
+    </TouchableOpacity>
 );
 
 export default listItem;
